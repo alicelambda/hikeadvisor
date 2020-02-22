4d142a9bff25c10eb7437aa3fbf9ffc0d6a04382
Alice Reuter <alice@alicereuter.com>
2020-02-22 15:04:22 -0600

    added default react app to repo

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import About from './About'
+import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+
+function App() {
+  return (
+    <div className="App">
+      <MuiThemeProvider>
+
+      </MuiThemeProvider>
+    </div>
+  );
+}
+
+export default App;
