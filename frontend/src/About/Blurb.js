8858672c1f059f86be176aafb0153655055caf88
Alice Reuter <alice@alicereuter.com>
2020-02-22 17:06:01 -0600

    adde Box components to style our blurbs

--- frontend/src/About/Blurb.js
+++ frontend/src/About/Blurb.js
@@ -3,9 +3,8 @@ import Avatar from '@material-ui/core/Avatar';
 import { makeStyles } from '@material-ui/core/styles';
 import { Typography } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
-import Card from '@material-ui/core/Card';
 import Paper from '@material-ui/core/Paper';
-
+import Box from '@material-ui/core/Box';
 const useStyles = makeStyles(theme => ({
     root: {
       display: 'flex',
@@ -25,16 +24,27 @@ const useStyles = makeStyles(theme => ({
 
 export default function Blurb() {
     const classes = useStyles();
+   
     return (
       
-        <div className={classes.root} >
-           <Grid item xs={12} spacing={3}>
-          <Paper>
-                <Avatar alt="Remy Sharp" src="/face.jpg" className={classes.large} />
-                <Typography variant="h5" component="h2" id="blurbtitle">
-                  Alice Reuter (Frontend)
-                </Typography>
-                <p>React Developer and cat memes enthusiast.</p>
+        <div className={classes.root}  >
+           <Grid item xs={12} spacing={2}>
+          <Paper >
+            <Box p={2}>
+                <Grid container spacing={2}>
+                  <Grid item>
+                    <Avatar alt="Remy Sharp" src="/face.jpg" className={classes.large} />
+                  </Grid>
+                  <Grid item>
+                    <Typography variant="h5" component="h2" id="blurbtitle">
+                      Alice Reuter (Frontend)
+                    </Typography>
+                    </Grid>
+                </Grid>
+              <Typography variant="body1" component="h2" id="blurbtitle">
+              React Developer and cat memes enthusiast.
+              </Typography>
+            </Box>
           </Paper>
           </Grid>
         </div>
